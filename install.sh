498449f3d40f76e72e94beba0e80c4ef4d6c5618
evan <evan@evanw.org>
2020-12-18 01:27:32 -0600

    update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 mkdir -p /home/root/bin
 cd /home/root/bin
-curl -LO http://github.com/evidlo/remarkable_printer/releases/latest/download/release.zip
-unzip release.zip
+wget -O release.zip http://github.com/evidlo/remarkable_printer/releases/latest/download/release.zip
+unzip -o release.zip
 mv printer.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable --now printer.service
