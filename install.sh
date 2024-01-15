51ee8e4bc151974299a7191109473134b021cffe
Jonas Große Sundrup <jonas-git@grosse-sundrup.com>
2024-01-15 14:46:30 -0500

    Update the install script to ensure it works with recent project state

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 systemctl stop printer.service || true
+systemctl stop printer.socket || true
 mkdir -p /home/root/bin
 cd /home/root/bin
 wget -O release.zip http://github.com/evidlo/remarkable_printer/releases/latest/download/release.zip
